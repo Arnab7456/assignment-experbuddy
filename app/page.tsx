9c6ecc3cc42eb31004c995f5352b21560b3d1695
arnav <arnabdas2753@gmail.com>
2025-03-30 17:59:26 +0530

    feat : TrustPage ui badge is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import HomeworkHelpLanding from "@/components/Hero";
+import TrustBadges from "@/components/TrustPage";
 export default function Home() {
   return (
-    <>
+    <div className="bg-[#F5F3EF]">
       <Navbar /> 
       <HomeworkHelpLanding />
-    </>
+      <TrustBadges />
+    </div>
   );
 }
